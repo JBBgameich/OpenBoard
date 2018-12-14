9eb88755349d18d7c93d7608d0b794d100f1549e
Jonah Brüchert <jbb.prv@gmx.de>
2018-12-14 11:38:13 +0100

    Add .gitlab-ci.yml

---
+++ ci_scripts/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if command -v sudo; then
+        export SUDO=$(command -v sudo)
+fi
+
+# Set up ccache
+export PATH=/usr/lib/ccache/:$PATH
+export CCACHE_BASEDIR=${PWD}
+export CCACHE_DIR=${PWD}/ccache
+if [ -d $CCACHE_DIR ]; then
+	$SUDO chown -R $(whoami):$(whoami) $CCACHE_DIR
+else
+	mkdir -p $CCACHE_DIR
+fi
+
+mkdir -p QtWebKit && touch QtWebKit/QWebView
+QT_SELECT=5 qmake CONFIG+=ccache
+make
