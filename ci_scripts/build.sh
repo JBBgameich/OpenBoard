8110476c755aac57a3bd948e67a448a42ced8cb0
Jonah Brüchert <jbb.prv@gmx.de>
2019-07-18 18:34:10 +0200

    Update ci_scripts for cmake

--- ci_scripts/build.sh
+++ ci_scripts/build.sh
@@ -4,12 +4,6 @@ if command -v sudo; then
         export SUDO=$(command -v sudo)
 fi
 
-if command -v qmake; then
-        export QMAKE=$(command -v qmake)
-elif command -v qmake-qt5; then
-        export QMAKE=$(command -v qmake-qt5)
-fi
-
 # Set up ccache
 export PATH=/usr/lib/ccache/:$PATH
 export CCACHE_BASEDIR=${PWD}
@@ -20,6 +14,5 @@ else
 	mkdir -p $CCACHE_DIR
 fi
 
-mkdir -p QtWebKit && touch QtWebKit/QWebView
-QT_SELECT=5 $QMAKE CONFIG+=ccache
+cmake .
 make
