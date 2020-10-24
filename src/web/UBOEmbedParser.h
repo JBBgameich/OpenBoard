359f3c97c6571e1facc25441e54bce25e5662806
Jonah Brüchert <jbb.prv@gmx.de>
2020-10-25 00:55:42 +0200

    Start restoring the OEmbed functionality

--- src/web/UBOEmbedParser.h
+++ src/web/UBOEmbedParser.h
@@ -85,7 +85,6 @@ public:
     UBOEmbedParser(QObject* parent=nullptr, const char* name="UBOEmbedParser");
     ~UBOEmbedParser() override;
     void parse(const QString& html);
-    void setNetworkAccessManager(QNetworkAccessManager* nam);
 
 signals:
     void parseContent(QString url);
@@ -93,7 +92,7 @@ signals:
 
 private slots:
     void onFinished(QNetworkReply* reply);
-    void onParseContent(QString url);
+    void onParseContent(const QString &url);
 
 private:
     sOEmbedContent getJSONInfos(const QString& json);
